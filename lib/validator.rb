41da2e0744be6693b04d24765ba0cdd8f23d1a47
Ņikita Peškovs <kipron11@gmail.com>
2023-05-19 14:59:53 +0300

    first commit

---
+++ lib/validator.rb
@@ -0,0 +1,14 @@
+class Validator
+  def initialize(puzzle_string)
+    @puzzle_string = puzzle_string
+  end
+
+  def self.validate(puzzle_string)
+    new(puzzle_string).validate
+  end
+
+  def validate
+
+  end
+
+end
